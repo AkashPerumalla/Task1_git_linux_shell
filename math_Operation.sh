d2d5ab363be2a21de04ad00f5f47e0c0b36de74f
Akash Perumalla <19jj1a0436@cej.jntuh.ac.in>
2024-04-07 12:36:06 +0530

    modifications in math_Operation.sh tax_salary.sh files in master

--- math_Operation.sh
+++ math_Operation.sh
@@ -6,7 +6,7 @@ read a
 echo "enter the num2: "
 read b
 
-echo -e "Select any choice\n 1.Add\n 2.Substract\n 3.Multiply\n 4.Divide"
+echo -e "Select any choice\n 1.Add\n 2.Substract\n 3.Multiply\n 4.Divide\n 5.Mod"
 read c
 
 case "$c" in
@@ -17,5 +17,7 @@ case "$c" in
 "3") echo "Multiplicaton of $a and $b:" $((a*b))
 	;;
 "4") echo "Division of $a and $b:" $((a/b))
+	;;
+"5") echo "Modulus of $a and $b:" $((a%))
 	;;
 esac
--- tax_salary.sh
+++ tax_salary.sh
@@ -3,9 +3,12 @@
 echo "enter your salary: "
 read sal
 
-if [ $sal -le 20000 ]
+if [ $sal -lt 20000 ]
 then
 	echo "no tax"
+elif [ $sal -eq 20000 ]
+then
+	echo "2% of $sal is $((sal*2/100))"
 elif [ $sal -gt 20000 -a $sal -le 40000 ]
 then
 	echo "5% of $sal is $((sal*5/100))"
